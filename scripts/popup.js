63a87c8a698a65b34de0d3bd939f539a2db820e5
jeremy <projeremylegendre@gmail.com>
2023-12-01 16:17:23 +0100

    rewriting functions

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,29 +1,46 @@
 console.log("popup.js");
-let police;
-let htmlElement;
 const btn = document.querySelector("button");
-const getHtmlElementValue = () => {
-  let selectHtmlElement = document.querySelector("#choixTag");
+//1 créer toute les variables
+let police1;
+let htmlElement1;
+let police2;
+let htmlElement2;
+//2 modifier les methodes ajout de parametre get HtmlElementValue getTypoValue
+const getHtmlElementValue = (selecteur) => {
+  let selectHtmlElement = document.querySelector(selecteur);
   let selectedHtmlElement = selectHtmlElement.value;
   return selectedHtmlElement;
 };
-const getTypoValue = () => {
-  let selectPolice = document.querySelector("#choixPolice");
+const getTypoValue = (police) => {
+  let selectPolice = document.querySelector(police);
   let selectedPolice = selectPolice.value;
   return selectedPolice;
 };
+//5 passer le message
 const sendMessage = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
-      font: police,
-      tag: htmlElement,
+      font: police1,
+      tag: htmlElement1,
     });
   });
 };
+//4 recupérer les valeurs
 btn.addEventListener("click", () => {
-  police = getTypoValue();
-  htmlElement = getHtmlElementValue();
-  sendMessage();
-  console.log(police + "police");
-  console.log(htmlElement + "tag");
+  police1 = getTypoValue("#choixPolice");
+  htmlElement1 = getHtmlElementValue("#choixTag");
+  police2 = getTypoValue("#SecondchoixPolice");
+  htmlElement2 = getHtmlElementValue("#SecondchoixTag");
+  //creer le logique pour éviter le doublon de tag html
+  if (htmlElement1 == htmlElement2) {
+    console.log("les 2 tags html sont identiques");
+    const divBtn = document.querySelector(".btnSendContainer");
+  } else {
+    console.log("envoi le message à la page");
+    // sendMessage();
+  }
+  console.log(police1 + "police1");
+  console.log(htmlElement1 + "tag1");
+  console.log(police2 + "police2");
+  console.log(htmlElement2 + "tag2");
 });
